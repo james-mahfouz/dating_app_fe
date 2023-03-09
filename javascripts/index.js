8e51019c0ae38792066ade83e1e203655c50da44
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-09 17:14:16 +0200

    finished the change suggestions function

--- javascripts/index.js
+++ javascripts/index.js
@@ -1,11 +1,20 @@
 window.onload = function(id) {
     
-    image=document.querySelector('.user_image img')
-    sugg_name = document.querySelector('#sugg_name')
-    sugg_location = document.querySelector('#sugg_location')
-    sugg_gender = document.querySelector('#sugg_gender')
-    sugg_bio = document.querySelector('#sugg_bio')
-    console.log(image, sugg_name, sugg_location, sugg_gender, sugg_bio)
+
+    change_suggestions("images/man.png", "John Smith", "Beirut", "male", "hello world")
+    function change_suggestions(url, name, location, gender, bio){
+        image=document.querySelector('.user_image img')
+        sugg_name = document.querySelector('#sugg_name')
+        sugg_location = document.querySelector('#sugg_location')
+        sugg_gender = document.querySelector('#sugg_gender')
+        sugg_bio = document.querySelector('#sugg_bio')
+
+        image.src = url
+        sugg_name.textContent = name
+        sugg_location.textContent = location
+        sugg_gender.textContent = gender
+        sugg_bio.textContent = bio
+    }
     
     function create_match(id, name, age){
         const left_bottom = document.querySelector('.left_bottom')
