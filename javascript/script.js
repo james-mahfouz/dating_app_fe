312f0d703db475dbb1ab7370f4a81abc0687abec
James Mahfouz <jamesmahfouz0@gmail.com>
2023-03-19 11:35:09 +0200

    added the post api the get the users

--- javascript/script.js
+++ javascript/script.js
@@ -90,6 +90,37 @@ workshop_pages.load_index = async () => {
     let user = JSON.parse(localStorage.getItem('user'));
     let username = document.getElementById('username')
     username.innerHTML += user.name
+
+    const data = new FormData()
+    data.append('gender', genders_id)
+    const response = await workshop_pages.postAPI(workshop_pages.base_url + 'getusers', data, token)
+    console.log(response)
+
+    dislike= document.querySelector(".dislike-icon")
+    like= document.querySelector(".like-icon")
+
+    function change_suggestions(url, name, location, gender, bio){
+        image=document.querySelector('.user_image img')
+        sugg_name = document.querySelector('#sugg_name')
+        sugg_location = document.querySelector('#sugg_location')
+        sugg_gender = document.querySelector('#sugg_gender')
+        sugg_bio = document.querySelector('#sugg_bio')
+
+        const location_icon = document.createElement("i")
+        location_icon.classList.add("fa-solid", "fa-location-dot")
+        const gender_icon = document.createElement("i")
+        gender_icon.classList.add("fa-solid", "fa-mars-and-venus")
+
+        image.src = url
+        sugg_name.textContent =  name
+        sugg_location.innerHTML = ""
+        sugg_location.appendChild(location_icon) 
+        sugg_location.innerHTML += location 
+        sugg_gender.innerHTML = ""
+        sugg_gender.appendChild(gender_icon)
+        sugg_gender.innerHTML += gender
+        sugg_bio.textContent = bio
+    }
 }
 
 workshop_pages.load_profile = async () =>{
